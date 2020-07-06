0b98d27936d393203c53bad7180aa500aabed368
chuukunn <atsumi.c.aa@m.titech.ac.jp>
2020-07-06 16:25:06 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -5,13 +5,32 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	ub = 1000000000;
+	lb = 0;
+	int count = 0;
+	int all = 0;
+	while (ub - lb > 1) {
+		all = 0;
+		count = 0;
+		int mid = (lb + ub) / 2;
+		for (i = 0; i < n; i++) {
+			count = A[i] / mid;
+			all = all + count;
+		}
+		if (all < k) {
+			ub = mid;
+		}
+		else {
+			lb = mid;
+		}
 
-
-  return 0;
+	}
+	printf("%d\n", lb);
+	return 0;
 }
