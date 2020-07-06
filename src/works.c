740cdc7a67f960ccddcedd8a028571bc1268a1f6
chuukunn <atsumi.c.aa@m.titech.ac.jp>
2020-07-06 16:25:23 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -5,13 +5,38 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	ub = 1000000000;
+	lb = 0;
+	int count = 0;
+	while (ub - lb > 1) {
+		int timepoint = 0;
+		int mid = (lb + ub) / 2;
+		for (i = 0; i < k; i++) {
+			count = 0;
+			while (count <= mid && A[timepoint] != 0) {
 
+				count = count + A[timepoint];
+				if (count <= mid) {
+					timepoint = timepoint + 1;
+				}
 
-  return 0;
+			}
+		}
+
+		if (timepoint >= n) {
+			ub = mid;
+		}
+		else {
+			lb = mid;
+		}
+
+	}
+	printf("%d\n", ub);
+	return 0;
 }
