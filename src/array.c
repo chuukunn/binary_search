c428e13f0cfac716b613eec349a60a54f19a092b
chuukunn <atsumi.c.aa@m.titech.ac.jp>
2020-07-06 16:24:43 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -5,14 +5,24 @@ int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	ub = n;
+	lb = 0;
 
-
-
-  return 0;
+	while (ub - lb > 1) {
+		int mid = (ub + lb) / 2;
+		if (A[mid] >= k) {
+			ub = mid;
+		}
+		else {
+			lb = mid;
+		}
+	}
+	printf("%d\n", ub);
+	return 0;
 }
